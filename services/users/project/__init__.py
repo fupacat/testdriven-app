8f099b8dc46fce186f4cd52f7677db8ed85f3b39
Eric Lunde <fupacat@gmail.com>
2018-12-08 00:38:45 -0500

    added flask migrtions

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -7,14 +7,17 @@ from flask import Flask  # new
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension  # new
 from flask_cors import CORS
+from flask_migrate import Migrate
 
 
 # instantiate the db
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()  # new
-
+migrate = Migrate()
 
 # new
+
+
 def create_app(script_info=None):
 
     # instantiate the app
@@ -29,6 +32,7 @@ def create_app(script_info=None):
     # set up extensions
     db.init_app(app)
     toolbar.init_app(app)  # new
+    migrate.init_app(app, db)
 
     # register blueprints
     from project.api.users import users_blueprint
