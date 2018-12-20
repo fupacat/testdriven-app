698ea922e342449273c4fb7aa73de3002700149d
Eric Lunde <fupacat@gmail.com>
2018-12-20 16:39:19 -0500

    added ecr into the ci process

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -9,28 +9,22 @@ import NavBar from './components/NavBar';
 import Form from './components/forms/Form';
 import Logout from './components/Logout';
 import UserStatus from './components/UserStatus';
+import Message from './components/Message';
 
 class App extends Component {
   constructor() {
       super();
       this.state = {
         users: [],
-        // username: '',
-        // email: '',
         title: 'TestDriven.io',
-        // formData: {
-        //   username: '',
-        //   email: '',
-        //   password: ''
-        // },
         isAuthenticated: false,
+        messageName: null,
+        messageType: null,
     };
-    // this.addUser = this.addUser.bind(this);
-    // this.handleChange = this.handleChange.bind(this);
-    // this.handleFormChange = this.handleFormChange.bind(this);
-    // this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
     this.logoutUser = this.logoutUser.bind(this);
     this.loginUser = this.loginUser.bind(this);
+    this.createMessage = this.createMessage.bind(this);
+    this.removeMessage = this.removeMessage.bind(this);
   };
   componentDidMount() {
     this.getUsers();
@@ -47,57 +41,6 @@ class App extends Component {
     })
     .catch((err) => { console.log(err); });
   };
-  // addUser(event) {
-  //   event.preventDefault();
-  //   const data = {
-  //     username: this.state.username,
-  //     email: this.state.email
-  //   };
-  //   axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
-  //   .then((res) => {
-  //     this.getUsers();
-  //     this.setState({ username: '', email: '' });
-  //   })
-  //   .catch((err) => { console.log(err); });
-  // };
-  // handleUserFormSubmit(event) {
-  //   event.preventDefault();
-  //   const formType = window.location.href.split('/').reverse()[0];
-  //   let data = {
-  //     email: this.state.formData.email,
-  //     password: this.state.formData.password,
-  //   };
-  //   if (formType === 'register') {
-  //     data.username = this.state.formData.username;
-  //   }
-  //   const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`
-  //   axios.post(url, data)
-  //   .then((res) => {
-  //     this.clearFormState();
-  //     window.localStorage.setItem('authToken', res.data.auth_token);
-  //     this.setState({ isAuthenticated: true, });
-  //     this.getUsers();
-  //   })
-  //   .catch((err) => { console.log(err); });
-  // };
-  // handleFormChange(event) {
-  //   const obj = this.state.formData;
-  //   obj[event.target.name] = event.target.value;
-  //   this.setState(obj);
-  //   console.log(obj);
-  // };
-  // handleChange(event) {
-  //   const obj = {};
-  //   obj[event.target.name] = event.target.value;
-  //   this.setState(obj);
-  // };
-  // clearFormState() {
-  //   this.setState({
-  //     formData: { username: '', email: '', password: ''},
-  //     username: '',
-  //     email: ''
-  //   });
-  // };
   logoutUser() {
     window.localStorage.clear();
     this.setState({ isAuthenticated: false });
@@ -106,7 +49,23 @@ class App extends Component {
     window.localStorage.setItem('authToken', token);
     this.setState({ isAuthenticated: true });
     this.getUsers();
-  }
+    this.createMessage('Welcome!', 'success');
+  };
+  createMessage(name='Sanity Check', type='success') {
+    this.setState({
+      messageName: name,
+      messageType: type
+    });
+    setTimeout(() => {
+      this.removeMessage();
+    }, 3000);
+  };
+  removeMessage() {
+    this.setState({
+      messageName: null,
+      messageType: null
+    });
+  };
   render() {
     return (
       <div>
@@ -116,6 +75,13 @@ class App extends Component {
         />
         <section className="section">
           <div className="container">
+            {this.state.messageName && this.state.messageType &&
+              <Message
+                messageName={this.state.messageName}
+                messageType={this.state.messageType}
+                removeMessage={this.removeMessage}
+              />
+            }
             <div className="columns">
               <div className="column is-half">
                 <br/>
@@ -128,21 +94,17 @@ class App extends Component {
                   <Route exact path='/register' render={() => (
                     <Form
                       formType={'Register'}
-                      // formData={this.state.formData}
-                      // handleFormChange={this.handleFormChange}
-                      // handleUserFormSubmit={this.handleUserFormSubmit}
                       isAuthenticated={this.state.isAuthenticated}
                       loginUser={this.loginUser}
+                      createMessage={this.createMessage}
                     />
                   )} />
                   <Route exact path='/login' render={() => (
                     <Form
                       formType={'Login'}
-                      // formData={this.state.formData}
-                      // handleFormChange={this.handleFormChange}
-                      // handleUserFormSubmit={this.handleUserFormSubmit}
                       isAuthenticated={this.state.isAuthenticated}
                       loginUser={this.loginUser}
+                      createMessage={this.createMessage}
                     />
                   )} />
                   <Route exact path='/about' component={About}/>
