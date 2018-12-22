484550cddd521c47cc003f4f61e293574036ddb1
Eric Lunde <fupacat@gmail.com>
2018-12-21 23:46:47 -0500

    asdf

--- docker-pull.sh
+++ docker-pull.sh
@@ -16,24 +16,24 @@ then
     export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
   fi
 
-  if [ "$TRAVIS_BRANCH" == "staging" ] || [ "$TRAVIS_BRANCH" == "production" ]
-  then
-    # docker pull $REPO/$USERS:$TAG
-    # # users
-    # docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-prod
-    # docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
-    # docker push $REPO/$USERS:$TAG
-    # # users db
-    # docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
-    # docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
-    # docker push $REPO/$USERS_DB:$TAG
-    # # client
-    # docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-prod --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
-    # docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
-    # docker push $REPO/$CLIENT:$TAG
-    # # swagger
-    # docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-prod
-    # docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
-    # docker push $REPO/$SWAGGER:$TAG
-  fi
+  # if [ "$TRAVIS_BRANCH" == "staging" ] || [ "$TRAVIS_BRANCH" == "production" ]
+  # then
+  #   # docker pull $REPO/$USERS:$TAG
+  #   # # users
+  #   # docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-prod
+  #   # docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
+  #   # docker push $REPO/$USERS:$TAG
+  #   # # users db
+  #   # docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
+  #   # docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
+  #   # docker push $REPO/$USERS_DB:$TAG
+  #   # # client
+  #   # docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-prod --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
+  #   # docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
+  #   # docker push $REPO/$CLIENT:$TAG
+  #   # # swagger
+  #   # docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-prod
+  #   # docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
+  #   # docker push $REPO/$SWAGGER:$TAG
+  # fi
 fi
