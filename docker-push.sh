84c2b22e02608c58545bdfd30d5315e1b1e5dbc4
Eric Lunde <fupacat@gmail.com>
2018-12-21 21:35:53 -0500

    added pull from ecr repo to build step

--- docker-push.sh
+++ docker-push.sh
@@ -30,14 +30,17 @@ then
     docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
     docker push $REPO/$USERS:$TAG
     # users db
+    docker pull $REPO/$USERS_DB:$TAG
     docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
     docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
     docker push $REPO/$USERS_DB:$TAG
     # client
+    docker pull $REPO/$CLIENT:$TAG
     docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-prod --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
     docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
     docker push $REPO/$CLIENT:$TAG
     # swagger
+    docker pull $REPO/$SWAGGER:$TAG
     docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-prod
     docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
     docker push $REPO/$SWAGGER:$TAG
