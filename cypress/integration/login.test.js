110d8c47b6abef56101c29278e93dfc829ae2b1b
Eric Lunde <fupacat@gmail.com>
2019-01-08 13:27:09 -0500

    added exercise interface and tests

--- cypress/integration/login.test.js
+++ cypress/integration/login.test.js
@@ -38,25 +38,39 @@ describe('Login', () => {
       .wait(100);
 
     // assert user is redirected to '/'
-    // assert '/' is displayed properly
+    cy.get('.notification.is-success').contains('Welcome!');
+    cy.contains('Users').click();
+    // assert '/all-users' is displayed properly
+    cy.get('.navbar-burger').click();
+    cy.location().should((loc) => { expect(loc.pathname).to.eq('/all-users') });
     cy.contains('All Users');
     cy
       .get('table')
       .find('tbody > tr').last()
       .find('td').contains(username);
-    cy.get('.notification.is-success').contains('Welcome!');
     cy.get('.navbar-burger').click();
-    cy.get('.navbar-menu').within(() => {
+    cy.get('.navbar-menu').within() => {
       cy
         .get('.navbar-item').contains('User Status')
         .get('.navbar-item').contains('Log Out')
         .get('.navbar-item').contains('Log In').should('not.be.visible')
         .get('.navbar-item').contains('Register').should('not.be.visible');
     });
+    // cy.get('.notification.is-success').contains('Welcome!');
+    // cy.get('.navbar-burger').click();
+    // cy.get('.navbar-menu').within(() => {
+    //   cy
+    //     .get('.navbar-item').contains('User Status')
+    //     .get('.navbar-item').contains('Log Out')
+    //     .get('.navbar-item').contains('Log In').should('not.be.visible')
+    //     .get('.navbar-item').contains('Register').should('not.be.visible');
+    // });
 
     // log a user out
-    cy.get('.navbar-burger').click()
-    cy.contains('Log Out').click();
+    // cy.get('.navbar-burger').click()
+    cy
+      .get('a').contains('Log Out').click();
+    // cy.contains('Log Out').click();
 
     // assert '/logout' is displayed properly
     cy.get('p').contains('You are now logged out');
