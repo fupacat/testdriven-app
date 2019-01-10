560796adb3a6c66d5f32c2cca3d590cbdb49bb5e
Eric Lunde <fupacat@gmail.com>
2019-01-10 13:17:32 -0500

    hopefully fixed some problems with cypress

--- services/users/entrypoint.sh
+++ services/users/entrypoint.sh
@@ -8,6 +8,11 @@ done
 
 echo "PostgreSQL started"
 
+<<<<<<< HEAD
 python manage.py recreate_db
 python manage.py seed_db
+=======
+python manage.py recreate-db
+python manage.py seed-db
+>>>>>>> hopefully fixed some problems with cypress
 gunicorn -b 0.0.0.0:5000 manage:app
